8c1244e3fbb8057d1e792bdce1494c35b97becac
Keerthana <Keerthana@DESKTOP-C6O517K>
2023-10-09 12:32:17 +0530

    Update

--- ISO8583/EmvTags.cs
+++ ISO8583/EmvTags.cs
@@ -318,5 +318,7 @@ class EmvTags
         // Find the tag with the specified tagId and return its Name property
         var tag = emvTags.Find(t => t.Id == tagId);
         return tag?.Name ?? "";
+       
     }
 }
+
